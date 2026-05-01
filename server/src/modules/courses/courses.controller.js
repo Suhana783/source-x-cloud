b55602a3eaedcc8065209c6d27d4bf98cf9457b1
Suhana783 <suhanachauhan133@gmail.com>
2026-05-01 18:29:46 +0530

    courses : model.js added

--- server/src/modules/courses/courses.controller.js
+++ server/src/modules/courses/courses.controller.js
@@ -1,37 +1,17 @@
-const courseService = require('./courses.services');
-const asyncHandler = require('../../Utility/asyncHandler');
+const Course = require('./courses.model');
 
-
-exports.createCourse = asyncHandler(async (req, res, next) => {
-    const course = await courseService.createCourseService(req.body);
+exports.createCourse = async (req, res, next) => {
+    
+try {
+    const data = req.body
+    const course = await Course.create(data);
 
     res.status(201).json({
         success: true,
-        message: "Course created successfully",
         data: course
     });
-});
+  } catch (error) {
+    next(error);
+  }
 
-exports.getAllCourses = asyncHandler(async (req, res, next) => {
-    const courses = await courseService.getAllCoursesService();
-
-    res.status(200).json({
-        success: true,
-        count: courses.length,
-        data: courses
-    });
-});
-
-
-exports.getCourseById = asyncHandler(async (req, res, next) => {
-    const course = await courseService.getCourseByIdService(req.params.id);
-
-    
-    if (!course) {
-        const error = new Error("Course not found");
-        error.statusCode = 404;
-        throw error;
-    }
-
-    res.status(200).json({ success: true, data: course });
-});
+};
